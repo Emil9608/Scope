d796aeefb63e91c9e6599d132a748ef26ae31c1b
Emil9608 <emil__96@hotmail.com>
2026-05-27 14:36:40 +0200

    First flyway migration

---
+++ backend/app/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    full_name VARCHAR(255),
+
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now()
+);
